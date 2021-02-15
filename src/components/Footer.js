6896fee948a67006f998e6d788b92b6f8caefabc
matieuruth <matieu.ruth@gmail.com>
2021-02-15 15:16:34 -0500

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import breakpoint from '../breakpoints';
 
 const Footer = () => {
     return (
-      <footer>
-        
-      </footer>
+      <Container>
+        <ul>
+          <li>Gift cards</li>
+          <li>promotions</li>
+          <li>find a store</li>
+          <li>sign up for email</li>
+          <li>become a member</li>
+          <li>laced up journal</li>
+          <li>send us feedback</li>
+        </ul>
+      </Container>
     );
   }
   
   export default Footer;
+
+  const Container = styled.footer`
+  background-color: #111111;
+  color: #FFF;
+  padding: 50px 0;
+
+  li {
+    text-transform: uppercase;
+    list-style: none;
+    text-align: left;
+    padding: 10px 0;
+  }
+
+    @media only screen and ${breakpoint.device.tablet} {
+
+    }
+  }`
